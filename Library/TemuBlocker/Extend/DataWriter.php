50b687ad9f185727c7fb385584a86475b7a2a306
Serdar MSC <serdarmsc@gmail.com>
2026-02-18 09:16:41 +0300

    Increase message limit and enhance spam checks
    Updated message count limit and added additional keywords for spam detection.

--- Library/TemuBlocker/Extend/DataWriter.php
+++ Library/TemuBlocker/Extend/DataWriter.php
@@ -21,7 +21,7 @@ class TemuBlocker_Extend_DataWriter extends XFCP_TemuBlocker_Extend_DataWriter
         }
 
         // sadece 5 mesajdan az kullanıcı
-        if ($visitor['message_count'] >= 5)
+        if ($visitor['message_count'] >= 10)
         {
             return;
         }
@@ -32,10 +32,19 @@ class TemuBlocker_Extend_DataWriter extends XFCP_TemuBlocker_Extend_DataWriter
         {
             return;
         }
-      // "t\s*e\s*m\s*u" temu kelimesinin farklı yazılışları için. ilave kelme eklemek için "|" kullanabilirsiniz.
-        if (preg_match('/(t\s*e\s*m\s*u|acw274625|code|kampanya)/i', $title))
+        if (preg_match('/(t\s*e\s*m\s*u|acw274625|code|kampanya|Gutschein|acu729640|T_e_m_u)/i', $title))
         {
-            $this->error('Yeni üyeler başlıkta bu kelimeyi kullanamaz.', 'title');
+            // ölüm vuruşu modu
+            $userDw = XenForo_DataWriter::create('XenForo_DataWriter_User');
+            $userDw->setExistingData($visitor['user_id']);
+            // yıldırılan kullanıcı yap
+            $userDw->set('is_discouraged', 1);
+            // group değiştir (Yasaklı : 6)
+            $userDw->set('user_group_id', 6);
+            $userDw->save();
+
+            $this->error('Spam tespit edildi. Hesabınız kısıtlandı.', 'title');
+
         }
     }
 }
